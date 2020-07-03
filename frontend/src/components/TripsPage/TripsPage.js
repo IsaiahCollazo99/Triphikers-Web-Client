42082a8fb3102e87c1ca54e744e5130833392f00
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-07-03 10:31:16 -0400

    make getAllTrips call in TripsPage component

--- frontend/src/components/TripsPage/TripsPage.js
+++ frontend/src/components/TripsPage/TripsPage.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllTrips } from '../../util/apiCalls/getRequests';
 
 const TripsPage = () => {
+    const [ trips, setTrips ] = useState([]);
+    
+    const getTripsCall = async () => {
+        try {
+            const allTrips = await getAllTrips();
+            setTrips(allTrips);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    useEffect(() => {
+        getTripsCall();
+    }, [])
+    
     return (
         <div className="tripPage">
             Trips
