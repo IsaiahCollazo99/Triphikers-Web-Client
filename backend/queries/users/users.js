e62d27143c4d938641a7032ad13d12b6ae652f75
bsihame <sihamebazi@pursuit.org>
2020-07-03 23:31:19 -0400

    add users queries folder and file js. add createNewUser query

---
+++ backend/queries/users/users.js
@@ -0,0 +1,21 @@
+const db = require("../../db/db");
+
+module.exports {
+  const createNewUser = async (req, res, next) => {
+    try {
+      const { id, full_name, email, age, gender, bio, country_of_origin } = req.body;
+      let user = await db.one(
+        `INSERT INTO users(id, full_name, email, age, gender, bio, country_of_origin)
+      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [id, full_name, email, age, gender, bio, country_of_origin]
+      )
+      res.status(200).json({
+        status: "OK",
+        user,
+        message: "Create user."
+      })
+
+    } catch (error) {
+      next(error)
+    }
+  }
+}
