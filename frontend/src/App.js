700079ff98097e32c37781e481ebecc1eb0667ee
bsihame <sihamebazi@pursuit.org>
2020-07-04 08:58:55 -0400

    create Landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import TripsPage from './components/TripsPage/TripsPage';
 import { Switch, Route } from 'react-router-dom';
+import Landing from './components/General/Landing';
 
 function App() {
   return (
     <div className="App">
       <Switch>
+      <Route path="/">
+          <Landing/>
+        </Route>
         <Route path="/">
           <TripsPage />
         </Route>
---
+++ frontend/src/components/Login/SignUpForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+id, full_name, email, age, gender,
+  bio, country_of_origin
+export default function SignUpForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const []
+}
