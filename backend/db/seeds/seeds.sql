b62081624a4b9b5a979f354e95cd1af03b02b484
jovannil94 <jovanniluna@pursuit.org>
2020-07-06 18:35:56 -0400

    adds locations to db

--- backend/db/seeds/seeds.sql
+++ backend/db/seeds/seeds.sql
@@ -31,4 +31,7 @@ VALUES
     1, 'Puerto Rico', '10/01/2020', '10/10/2020', 'Any', 'English', 'Video Call', 
     'Explore Cities', 'Exploring Puerto Rico', 'Yes', 'Hotel', 1000, 'Yes', 'None',
     'Just want to explore around cities with my friends and some new people'
-)
+);
+
+INSERT INTO locations (id, image, latitude, longitude, location_name, emergency_services, poster_id)
+VALUES (1, 'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg', 51, 00, 'London', 999, 1);
