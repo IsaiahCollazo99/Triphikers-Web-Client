f6165845bd3e02197f278a45bacbbd6b56df175b
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-07-07 11:45:44 -0400

    update DetailedTripPage to fetch the trip. Create needed DetailedTrip Components

--- frontend/src/components/DetaliedTripPage/DetailedTripPage.js
+++ frontend/src/components/DetaliedTripPage/DetailedTripPage.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getTripById } from '../../util/apiCalls/getRequests';
 
 const DetailedTripPage = () => {
     const { id } = useParams();
-    
+    const [ trip, setTrip ] = useState({});
+
+    const getTripCall = async () => {
+        try {
+            const tripById = await getTripById(id);
+            setTrip(tripById.trip);
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+
+    useEffect(() => {
+        getTripCall();
+    }, [])
+
     return (
         <div className="detailedTripContainer">
-            {id}
+            <header></header>
+            <nav></nav>
+            
         </div>
     )
 }
