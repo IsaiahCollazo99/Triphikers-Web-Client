99f8a3b1fb86014dd5a9dc444bdcc4a4deba7d85
jovannil94 <jovanniluna@pursuit.org>
2020-07-13 10:43:18 -0400

    finishes search, adding markers to search and directions

--- frontend/src/components/helper/maps/AttractionsMap.js
+++ frontend/src/components/helper/maps/AttractionsMap.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import gspIcon from "../../../images/FindMeIcon.svg"
 import usePlacesAutocomplete, {
     getGeocode,
     getLatLng,
@@ -46,7 +47,7 @@ const AttractionsMap = ({ location, fetchData }) => {
 
     const panTo = React.useCallback(({lat, lng}) => {
         mapRef.current.panTo({lat, lng});
-        mapRef.current.setZoom(14);
+        mapRef.current.setZoom(16);
     } , []);
     
     if(loadError) return "Error loading maps";
@@ -58,6 +59,7 @@ const AttractionsMap = ({ location, fetchData }) => {
             <h1 className="mapTitle">Attractions <span role="img" aria-label="pin"> 📸</span></h1>
 
             <Search  panTo={panTo} markers={location}/>
+            <Locate panTo={panTo}/>
 
             <GoogleMap mapContainerStyle={mapContainerStyle} zoom={11} center={location} onClick={(e) => {setMarkers({
                 lat: e.latLng.lat(),
@@ -67,6 +69,18 @@ const AttractionsMap = ({ location, fetchData }) => {
                 <Marker position={{ lat: parseFloat(markers.lat), lng: parseFloat(markers.lng) }}/>
             </GoogleMap>
         </div>
+    )
+}
+
+const Locate = ({panTo}) => {
+    return(
+        <button className="locateMeButton" onClick={() => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                panTo({ lat: position.coords.latitude, lng: position.coords.longitude})
+            }, () => null)
+        }}>
+            <img className="gpsImg" src={gspIcon} alt="locate me"/>
+        </button>
     )
 }
 
@@ -80,19 +94,24 @@ const Search = ({panTo, markers}) => {
     return(
         <div className="search">
             <Combobox onSelect={async (address) => {
+                setValue(address, false);
+                clearSuggestions();
                     try {
                         const res = await getGeocode({address});
                         const { lat, lng } = await getLatLng(res[0]);
+                        console.log(address) //add driving directions here
                         panTo({ lat, lng })
                     } catch(error) {
                         console.log(error)
                     }
                 }}>
-                <ComboboxInput value={value} onChange={(e)=>{
+                <ComboboxInput className="searchInput" value={value} onChange={(e)=>{
                     setValue(e.target.value)
                 }} disabled={!ready} placeholder="Search An Attraction"/>
                 <ComboboxPopover>
-                    {status === "OK" && data.map(({description }, index) => <ComboboxOption key={index} value={description} className="searchResults"/>)}
+                    <ComboboxList>
+                        {status === "OK" && data.map(({description }, index) => <ComboboxOption key={index} value={description} className="searchResults"/>)}
+                    </ComboboxList>
                 </ComboboxPopover>
             </Combobox>
         </div>
