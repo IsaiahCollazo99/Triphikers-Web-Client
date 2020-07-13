42d80d5cdbf4a8ea57f21311f4eb518a69cd7fb2
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-07-13 04:57:26 -0400

    create signUpWithGoogle function

--- frontend/src/util/firebaseFunction.js
+++ frontend/src/util/firebaseFunction.js
@@ -1,6 +1,10 @@
 import firebase from "../firebase";
 import { storage } from 'firebase';
 
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+firebase.auth().useDeviceLanguage();
+
 export const logout = () => firebase.auth().signOut();
 
 export const login = (email, password) => {
@@ -9,6 +13,16 @@ export const login = (email, password) => {
 
 export const signUp = (email, password) => {
 	return firebase.auth().createUserWithEmailAndPassword(email, password);
+}
+
+export const signUpWithGoogle = async () => {
+    try {
+        await firebase.auth().signInWithRedirect(provider);
+        const result = await firebase.auth().getRedirectResult()
+        return result;
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const getFirebaseIdToken = () => {
