9c5adff1d3055e7aa75f1cd7b55c5c4e96b1b0c6
jovannil94 <jovanniluna@pursuit.org>
2020-07-14 11:14:36 -0400

    Adds zoom and recenter of hotspot map on select

--- frontend/src/components/helper/maps/HotspotMaps.js
+++ frontend/src/components/helper/maps/HotspotMaps.js
@@ -12,6 +12,8 @@ const mapContainerStyle = {
 const HotspotMap = ({ location, fetchData, allMarkers }) => {
     const [markers, setMarkers] = useState([]);
     const [selected, setSelected] = useState(null);
+    const [epicenter, setEpicenter] = useState(location);
+    const [zoom, setZoom] = useState(11);
 
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: apiKey,
@@ -27,7 +29,7 @@ const HotspotMap = ({ location, fetchData, allMarkers }) => {
                 selected: selected
             });
         }
-    }, [markers, selected, fetchData])
+    }, [markers, selected])
     
     
     if(loadError) return "Error loading maps";
@@ -37,13 +39,13 @@ const HotspotMap = ({ location, fetchData, allMarkers }) => {
     return(
         <div className="googleMaps">
             <h1 className="mapTitle">Hotspots <span role="img" aria-label="pin">📍</span></h1>
-            <GoogleMap mapContainerStyle={mapContainerStyle} zoom={11} center={location} onClick={(e) => {setMarkers({
+            <GoogleMap mapContainerStyle={mapContainerStyle} zoom={zoom} center={epicenter} onClick={(e) => {setMarkers({
                 lat: e.latLng.lat(),
                 lng: e.latLng.lng(),
             })}}>
 
                 {allMarkers.map((marker) => (
-                    <Marker key={marker.id} position={{ lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) }} onClick={() => {setSelected(marker)}}/>
+                    <Marker key={marker.id} position={{ lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) }} onClick={() => {setSelected(marker); setEpicenter({lat: parseFloat(marker.lat), lng: parseFloat(marker.lng)}); setZoom(16)}}/>
                 ))}
 
                 <Marker position={{ lat: parseFloat(markers.lat), lng: parseFloat(markers.lng) }}/>
