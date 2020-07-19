710dd51a76e34878b09a451f79a63ebc4bf45eb5
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-07-19 16:24:07 -0400

    add signUpCss1 and check to see if password and confirm password match

--- frontend/src/components/Login/CreateSignUpForm1.js
+++ frontend/src/components/Login/CreateSignUpForm1.js
@@ -1,12 +1,17 @@
-import React from "react";
-
+import React, { useState } from "react";
+import '../../css/signUpIn/signUp1.css';
 
 const CreateSignUpForm1 = (props) => {
 	const { email, password, confirmPassword, handlePageChange } = props;
+	const [ error, setError ] = useState(null);
 	
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		handlePageChange(2)
+		if(password.value !== confirmPassword.value) {
+			setError(<p className="error">The passwords don't match</p>)
+		} else {
+			handlePageChange(2)
+		}
 	}
 	return (
 		<>
@@ -15,17 +20,19 @@ const CreateSignUpForm1 = (props) => {
 			<h3>1/3</h3>
 		</header>
 
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit} className="signUp1">
+			{error}
+		
 			<label htmlFor="email">Email: </label>
 			<input type="email" {...email} name="email" autoComplete="on" required />
 
-			<label htmlFor="password">Password </label>
+			<label htmlFor="password">Password: </label>
 			<input type="password" {...password} name="password" autoComplete="on" required />
 
 			<label htmlFor="confirm">Confirm password: </label>
 			<input type="password" {...confirmPassword} name="confirm" autoComplete="on" required />
 
-			<input type="submit" value="continue" />
+			<input type="submit" value="Next Page" />
 		</form>
 		</>
 	);
