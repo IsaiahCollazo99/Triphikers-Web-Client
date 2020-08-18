1dcc493aea02d6fc66ce6d783b21b179ae238014
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-08-18 02:57:01 -0400

    userPage css

--- frontend/src/components/User/UserPage.js
+++ frontend/src/components/User/UserPage.js
@@ -41,7 +41,7 @@ const UserPage = () => {
         return () => {
             setProfileUser(null);
         }
-    }, []);
+    }, [id]);
 
     useEffect(() => {
         if(profileUser) {
@@ -60,33 +60,31 @@ const UserPage = () => {
       
         <div className="userPageContainer">
             <header>
+                <section className="up-coverImage">
+                </section>
+
+                <section className="up-user">
+                    <img src={profileUser.profile_picture} alt="profile_picture" />
+                    <div className="up-userInteraction">
+                        {/* rating goes in the span */}
+                        <p>{profileUser.full_name}<span></span></p>
+                        <section>
+                            {/* Social Media goes here */}
+                        </section>
+                    </div>
+                </section>
+
+                <section className="up-userInfo">
+                    <p><span>Age: </span>{profileUser.age}</p>
+                    <p><span>Country of Origin: </span>{profileUser.country_of_origin}</p>
+                    <p><span>Gender: </span>{profileUser.gender}</p>
+                </section>
+
+                <section className="up-bio">
+                    <span>Bio: </span>
+                    <p>{profileUser.bio}</p>
+                </section>
             </header>
-
-            <section className="userProfile">
-                <header className="up-header">
-                    <section className="up-user">
-                        <img src={profileUser.profile_picture} alt="profile_picture" />
-                        <div className="up-userInteraction">
-                            {/* rating goes in the span */}
-                            <p>{profileUser.full_name}<span></span></p>
-                            <section>
-                                {/* Social Media goes here */}
-                            </section>
-                        </div>
-                    </section>
-
-                    <section className="up-userInfo">
-                        <p><span>Age: </span>{profileUser.age}</p>
-                        <p><span>Country of Origin: </span>{profileUser.country_of_origin}</p>
-                        <p><span>Gender: </span>{profileUser.gender}</p>
-                    </section>
-
-                    <section className="up-bio">
-                        <span>Bio: </span>
-                        <p>{profileUser.bio}</p>
-                    </section>
-                </header>
-            </section>
 
             <UserPageNav userId={profileUser.id} />
 
