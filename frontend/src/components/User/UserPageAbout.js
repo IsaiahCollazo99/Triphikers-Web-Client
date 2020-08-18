2901d6e2820e4539d525108782aaf65116f8d63b
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-08-18 08:22:13 -0400

    trip card css

--- frontend/src/components/User/UserPageAbout.js
+++ frontend/src/components/User/UserPageAbout.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import '../../css/userPage/About.css'
+import React, { useContext } from 'react';
+import '../../css/userPage/userPageAbout.css'
+import { AuthContext } from '../../providers/AuthContext';
 
 const UserPageAbout = ({ user }) => {
+    const { currentUser } = useContext(AuthContext);
+
+    const displayEdit = () => {
+        if(currentUser.id === user.id) {
+            return <button>Edit</button>
+        } else {
+            return null;
+        }
+    }
 
     return (
         <section className="up-about">
-            <button>Edit</button>
+            {displayEdit()}
             <p><span>Language: </span>{user.language}</p>
         </section>
 
