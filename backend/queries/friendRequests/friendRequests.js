5c534100005c6467b4310353878816b26868d73d
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-08-20 14:56:35 -0400

    finish sendFriendRequest query

--- backend/queries/friendRequests/friendRequests.js
+++ backend/queries/friendRequests/friendRequests.js
@@ -4,7 +4,16 @@ module.exports = {
     sendFriendRequest: async ( req, res, next ) => {
         try {
             const { requester_id, requested_id } = req.body;
-            
+            const friendRequest = await db.one(`
+                INSERT INTO friend_requests (requester_id, requested_id)
+                VALUES ($1, $2) RETURNING *
+            `, [requester_id, requested_id]);
+
+            res.status(200).json({
+                status: "OK",
+                friendRequest,
+                message: "Friend Request successfuly made."
+            })
         } catch ( error ) {
             next(error);
         }
