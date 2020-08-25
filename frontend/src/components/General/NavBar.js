691d4c0e8ee090de16a792285047b379b059b7ff
bsihame <sihamebazi@pursuit.org>
2020-08-25 01:46:39 -0400

    finish dropdown menue and  remove navbarcode

--- frontend/src/components/General/NavBar.js
+++ frontend/src/components/General/NavBar.js
@@ -1,138 +1,102 @@
-import React, { useContext, useState , useEffect} from 'react';
-import { NavLink, Link, useLocation } from 'react-router-dom';
-import '../../css/general/navBar.css';
-import { logout } from '../../util/firebaseFunction';
-import { AuthContext } from '../../providers/AuthContext';
-import firebase from "../../firebase";
-import Navbar from "../Navbar/Navbar";
-import NavItem from "../Navbar/NavItem"
-import {getUserById} from '../../util/apiCalls/getRequests';
-
-
+import React, { useContext, useState, useEffect } from "react";
+import { NavLink, Link, useLocation } from "react-router-dom";
+import "../../css/general/navBar.css";
+import { logout } from "../../util/firebaseFunction";
+import { AuthContext } from "../../providers/AuthContext";
+import { getUserById } from "../../util/apiCalls/getRequests";
+import "bootstrap/dist/css/bootstrap.min.css";
 const NavBar = () => {
-    const { currentUser } = useContext(AuthContext);
+	const { currentUser } = useContext(AuthContext);
 
-    const [firstName , setFirstName]= useState("")
-    const location = useLocation();
-    
-    
-    
-    const isOnTripsPage = location.pathname === "/trips";
-    const isOnCreateTripsPage = location.pathname === "/trips/create";
+	const [firstName, setFirstName] = useState("");
+	const location = useLocation();
 
-    const displayCreateTrip = () => {
-        if(isOnTripsPage || isOnCreateTripsPage) {
-            return null
-        } else {
-            return (
-                <NavLink to="/trips/create">CREATE A TRIP</NavLink>
-            )
-        }
-    }
-    
+	const isOnTripsPage = location.pathname === "/trips";
+	const isOnCreateTripsPage = location.pathname === "/trips/create";
 
-    // const fetchFirstName = () => {
-    //     firebase.auth().onAuthStateChanged(async user => {
-    //         debugger
-    //         setFirstName(user.displayName.split(" ")[0])
-    //     })
-    // }
-    // console.log(firstName)
+	const displayCreateTrip = () => {
+		if (isOnTripsPage || isOnCreateTripsPage) {
+			return null;
+		} else {
+			return <NavLink to="/trips/create">CREATE A TRIP</NavLink>;
+		}
+	};
 
-    const getFirstName = async () => {
-        const data = await getUserById(currentUser.id);
-        setFirstName(data.user.first_name);
-     
-    }
+	const getFirstName = async () => {
+		const data = await getUserById(currentUser.id);
+		setFirstName(data.user.first_name);
+	};
 
-    const displayNavBar = () => {
-        if(currentUser) {
-            return (
-                <section className="mainNav-right">
-                    {displayCreateTrip()}
-                    <NavLink exact to="/trips">TRIPS</NavLink>
-                    <NavLink to={`/user/${currentUser.id}`}>PROFILE</NavLink>
-                    <NavLink to="/search">CITY SEARCH</NavLink>
-                    <NavLink to="/messages">MESSAGES</NavLink>
-                    {/* <Navbar> */}
-                        {/* <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ml-auto">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle"
-                                        href="#"
-                                        id="navbarDropdown"
-                                        role="button" data-toggle="dropdown"
-                                        >Hi, {firstName}</a> */}
-                                    {/* >Hi, {firstName}</a> */}
-                        {/* <NavLink to={`/user/${firstName}`}
-                            className="NaveItem dropdown"
-                            href="#" id="navbarDropdown"
-                            role="button" data-toggle="dropdown"
-                                    >Hi, {firstName}</NavLink> */}
-                                {/* </li>
-                                
+	const displayNavBar = () => {
+		if (currentUser) {
+			return (
+				<section className="mainNav-right">
+					{displayCreateTrip()}
+					<NavLink exact to="/trips">
+						TRIPS
+					</NavLink>
+					<NavLink to={`/user/${currentUser.id}`}>PROFILE</NavLink>
+					<NavLink to="/search">CITY SEARCH</NavLink>
+					<NavLink to="/messages">MESSAGES</NavLink>
+					<a
+						className="nav-link dropdown-toggle"
+						data-toggle="dropdown"
+						href="/trips"
+						role="button"
+						aria-expanded="false"
+					>
+						Hi, {firstName}
+					</a>
+					<div className="dropdown-menu">
+						<a className="dropdown-item" href="/user/${currentUser.id}">
+							PROFILE
+						</a>
+						<div className="dropdown-divider"></div>
+						<a className="dropdown-item" href="/messages">
+							MESSAGES
+						</a>
+						<div className="dropdown-divider"></div>
+						<a className="dropdown-item" onClick={redirect}>
+							LOG OUT
+						</a>
+					</div>
+					<a onClick={redirect}>LOG OUT</a>
+				</section>
+			);
+		} else {
+			return (
+				<section className="lp-navRight">
+					<NavLink exact to="/">
+						ABOUT
+					</NavLink>
+					<NavLink to="/safety">SAFETY</NavLink>
+					<NavLink to="/signUp">CREATE ACCOUNT</NavLink>
+					<NavLink to="/signIn">LOG IN</NavLink>
+				</section>
+			);
+		}
+	};
 
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">PROFILE</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">PROFILE</a>
-                            
-                                </div>
-                                </ul>
-                    </div> */}
-                    {/* </Navbar> */}
+	const redirect = async () => {
+		await logout();
+	};
 
+	useEffect(() => {
+		getFirstName();
+	}, []);
 
-                    <div class="dropdown">
-  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-    Hi, {firstName}
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Profile</a>
-    <a class="dropdown-item" href="#">Profile2</a>
-    <a class="dropdown-item" href="#">Link 3</a>
-  </div>
-</div>
+	return (
+		<nav className="mainNav">
+			<section className="mainNav-left">
+				{/* Logo Here */}
+				<h1>
+					<Link to={currentUser ? "/trips" : "/"}>TRIPHIKERS</Link>
+				</h1>
+			</section>
 
-                    
-                    <a onClick={redirect}>LOG OUT</a>
-                </section>
-            )
-        } else {
-            return (
-                <section className="lp-navRight">
-                    <NavLink exact to="/">ABOUT</NavLink>
-                    <NavLink to="/safety">SAFETY</NavLink>
-                    <NavLink to="/signUp">CREATE ACCOUNT</NavLink>
-                    <NavLink to="/signIn">LOG IN</NavLink>
-                </section>
-            )
-        }
-    }
-
-    const redirect = async () => {
-        await logout();
-    }
-
-    useEffect(() => {
-        getFirstName();
-    }, [])
-
-
-    // useEffect(() => {
-    //     fetchFirstName()
-    // }, [])
-    
-    return (
-        <nav className="mainNav">
-            <section className="mainNav-left">
-                {/* Logo Here */}
-                <h1><Link to={currentUser ? "/trips" : "/"}>TRIPHIKERS</Link></h1>
-            </section>
-
-            {displayNavBar()}
-        </nav>
-    )
-}
+			{displayNavBar()}
+		</nav>
+	);
+};
 
 export default NavBar;
