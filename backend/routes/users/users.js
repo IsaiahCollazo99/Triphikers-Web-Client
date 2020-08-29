4325df6b5782f4d07ad5445ff1a62678f3e2b559
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-08-29 12:02:27 -0400

    add getUserByEmail

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -20,7 +20,7 @@ users.get("/:id/trips", getUserTrips);
 users.get("/:id/friendRequests", getUserFriendRequests);
 users.get("/:id/friends", getUsersFriends);
 users.get("/username/:username", getUserByUsername);
-users.get("/email", getUserByEmail);
+users.get("/email/:email", getUserByEmail);
 users.patch("/:id", updateUser);
 users.delete("/:id/friendRequests", deleteFriendRequest);
 users.delete("/:id/friends", deleteFriend);
