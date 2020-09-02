d90d9b4da08ff327faed2ce51e1ec1e23a60425f
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-02 18:28:17 -0400

    css

--- frontend/src/components/DetaliedTripPage/DetailedTripNav.js
+++ frontend/src/components/DetaliedTripPage/DetailedTripNav.js
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import '../../css/detailedTripPage/detailedTripNav.css';
 import { AuthContext } from '../../providers/AuthContext';
+import '../../css/detailedTripPage/detailedTripNav.css';
 
 const DetailedTripNav = ({ trip = {} }) => {
     const { currentUser } = useContext(AuthContext);
 
     const displayRequests = () => {
         if(currentUser.id === trip.planner_id) {
-            return <NavLink exact to={`/trips/${trip.id}/requests`}>Requests</NavLink>
+            return <NavLink exact to={`/trips/${trip.id}/requests`}>REQUESTS</NavLink>
         } else {
             return null;
         }
@@ -16,8 +16,8 @@ const DetailedTripNav = ({ trip = {} }) => {
     
     return (
         <nav className="dt-nav">
-            <NavLink exact to={`/trips/${trip.id}`}>Trip</NavLink>
-            <NavLink exact to={`/trips/${trip.id}/travelers`}>Travelers</NavLink>
+            <NavLink exact to={`/trips/${trip.id}`}>TRIP</NavLink>
+            <NavLink exact to={`/trips/${trip.id}/travelers`}>TRAVELERS</NavLink>
             {displayRequests()}
         </nav>
     )
