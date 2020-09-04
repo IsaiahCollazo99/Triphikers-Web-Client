440e407cfa5323965a10a788f9fcb9eb865254ab
jovannil94 <jovanniluna@pursuit.org>
2020-09-04 16:48:05 -0400

    creates and updates css files, working on new location layout

--- frontend/src/components/Location/LocationInfo.js
+++ frontend/src/components/Location/LocationInfo.js
@@ -9,7 +9,7 @@ import { createClient } from 'pexels';
 //not working and must do for weather API as well
 let weatherkey = '18d629f0d66c4d5e831121754202907';
 
-const LocationInfo = ({city, coord, country, title}) => {
+const LocationInfo = ({ city, coord, country }) => {
     const client = createClient(`563492ad6f9170000100000153f28b06267f4b548fc99fbb457455db`);
     // const [currency, setCurrency] = useState([]);
     const [travelAdv, setTravelAdv] = useState([]);
@@ -20,7 +20,6 @@ const LocationInfo = ({city, coord, country, title}) => {
     const getAllInfo = async (city, coord, country) => {
         if(city.length > 0){
             try {
-                debugger
                 // let currency = await axios.get(`https://api.exchangeratesapi.io/latest?symbols=USD,GBP`);
                 let travelAdvisory = await axios.get(`https://www.travel-advisory.info/api?countrycode=${country}`);
                 let weather = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${weatherkey}&q=${coord.lat},${coord.lng}&days=7`);
@@ -59,17 +58,19 @@ const LocationInfo = ({city, coord, country, title}) => {
         if(info !== undefined){
             return (
                 <div>
-                    <h1>Travel Advisory</h1>
-                    <h2>{country.name}</h2>
+                    <h1 className="locationTitle">Travel Advisory</h1>
                     <section className="li-labels">
-                        <label>Continent: </label>
-                        <p>{country.continent}</p>
-
-                        <label>Local Situation Rating: </label>
-                        <p>{info.score}</p>
+                        <div className="locationTravelAd">
+                            <label className="locationTitle"> Continent: </label>
+                            <p>{country.continent}</p>
+                        </div>
+                        <div className="locationTravelAd">
+                            <label className="locationTitle"> Local Situation Rating: </label>
+                            <p> {info.score}</p>
+                        </div>
                     </section>
                     <p className="linkAdv" onClick={()=>window.open(info.source)}>Click here for Advisory Information</p>
-                    <p>Information was updated on {info.updated}</p>
+                    <p className="locationTravelAdNotice">Information was updated on {info.updated}</p>
                 </div>
             )
         }
@@ -99,32 +100,23 @@ const LocationInfo = ({city, coord, country, title}) => {
         getAllInfo(city, coord, country);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [city])
-
+    
     return(
         <div className="locationInfoContainer">
-            <div className="locationImageContainer">
-                <img className="cityImage" src={imageRef} alt="location"/>
-                <div className="overlay">
-                    <div className="locationName">{title}</div>
-                </div>
+            <h1 className="locationTitle">{city}</h1>
+            <div className="locationInfoCard">
+                {advisoryPrint(travelAdv)}
             </div>
-            <div className="details">
-                <div className="detailsAdvisory">
-                    {advisoryPrint(travelAdv)}
-                </div>
-                <div className="weatherContainer">
-                    <h1 className="weatherTitle">Weather Forecast</h1>
-                    <div className="weatherForecast">
+            <div className="locationInfoCard">
+                <h1 className="locationTitle">Weather Forecast</h1>
+                <div className="weatherForecast">
                     {printForecast}
-                    </div>
                 </div>
             </div>
-            <div className="li-misc">
-                <div className="localTime">
-                    <label>Local Time: </label>
-                    <p>{convertTime(currentTime)}</p>
-                </div>  
-            </div>
+            <div className="locationInfoCard">
+                <label className="locationTitle">Local Time: </label>
+                <p>{convertTime(currentTime)}</p>
+            </div> 
                 {/* <div className="detailsCurrency">
                     <h1 className="locationPageText">Currency Exchange</h1>
                         {currencyPrint(currency)}
