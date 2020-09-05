60a8dde16af8f59d59a537ecd68d734f710b4a05
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-05 15:58:05 -0400

    css

--- frontend/src/components/User/UserPageAbout.js
+++ frontend/src/components/User/UserPageAbout.js
@@ -6,7 +6,10 @@ const UserPageAbout = ({ user }) => {
     return (
         <section className="up-about">
             <section className="up-aboutList">
-                <p><span>Language: </span>{user.language}</p>
+                <span>Language: </span><p>{user.language}</p>
+                <span>Age: </span><p>{user.age}</p>
+                <span>Country: </span><p>{user.country_of_origin}</p>
+                <span>Gender: </span><p>{user.gender}</p>
             </section>
         </section>
 
