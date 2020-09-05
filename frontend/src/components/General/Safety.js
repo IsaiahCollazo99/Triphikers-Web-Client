7b099946028dfefe3c64c4b744b7188679bfb51c
bsihame <sihamebazi@pursuit.org>
2020-09-05 01:02:26 -0400

    finish Safety page css

--- frontend/src/components/General/Safety.js
+++ frontend/src/components/General/Safety.js
@@ -1,190 +1,218 @@
-// import React from 'react';
-// import { withStyles, makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import "../../css/safety/Safety.css"
 
-
-// export default function Safety() {
-// 	const useStyles = makeStyles((theme) => ({
-// 		margin: {
-// 			margin: theme.spacing(1),
-// 		},
-// 	}));
-
-//   return (
-// 		<div className="safetyContainer">
-// 			<h2 className="StafetyTitle">Solo Travel Safety Precautions</h2>
-
-// 			<h2>Save Money</h2>
-// 			<p>
-// 				Each country has a high season, a low season, and in between
-// 				season.Those in between season are know as "shoulder seasons" and they
-// 				generally offer the ideal mix of affordability and comfort.Shoulder
-// 				seasons roughly coincide with spring and fall. the shoulder seasons are
-// 				the transitional periods between the rainy and dry seasons. Before you
-// 				make any decision of your destination, you should check the climate
-// 			</p>
-// 			<h2>Study Up on the Local Authorities</h2>
-
-// 			<p>
-// 				Before you visit any country, do some research into the trustworthiness
-// 				of local civil authorities like police, emergency services Before you
-// 				arrive at your destination, check whether the country or city you’re
-// 				visiting has tourist police and plan accordingly.
-// 			</p>
-// 			<h2>Purchase Travel Insurance</h2>
-// 			<p>
-// 				Travel insurance is great for groups large and small, but it’s
-// 				especially helpful for solo travelers. If you’re planning to travel
-// 				abroad, especially if your itinerary involves long-distance hiking in
-// 				remote areas or other risky activities, make sure your policy has
-// 				medical evacuation coverage. Medical airlifts (air ambulances) are
-// 				incredibly expensive.
-// 			</p>
-// 			<h2> Protect Your Money and Valuables</h2>
-// 			<p>
-// 				Protecting Your Money and Valuables The first rule of preventing theft
-// 				while traveling abroad: Never carry more money than you need at one
-// 				time. If you’re spending two weeks abroad, don’t carry a two-week supply
-// 				of cash every time you leave your room. The second rule: Secure your
-// 				money, cards, and valuables in something other than a back pocket or
-// 				flimsy wallet. For cash, get a money belt that straps securely to your
-// 				waist and can’t easily be separated from your clothing. Quality models
-// 				start at $15 apiece. For your cards, get an RFID-blocking wallet that
-// 				stymies EMV chip card scanners, which high-tech pickpockets use to lift
-// 				credit card data from passersby in public places. My RFID-blocking
-// 				wallet cost less than $20 and looks great – it’s not actual leather, but
-// 				I certainly can’t tell the difference.
-// 				</p>
-// 				<h2> Be Discreet With Accessories </h2>
-// 				<p>
-// 				Flashing shiny, spendy accessories is a surefire way to attract the
-// 				wrong kind of attention. Avoid displaying expensive-looking jewelry,
-// 				handbags, sunglasses, and other accessories on the road. In fact, unless
-// 				you’re traveling for business and need to dress to impress, there’s
-// 				little reason to even bring such accessories on your trip. If you do
-// 				need to sport name brands at an evening function or two, that’s fine –
-// 				just dress down for the rest of your trip and leave the expensive stuff
-// 				tucked safely away in your room.
-// 					</p>
-// 				<h2>Reserve Secure Room Locker </h2>
-// 				<p>
-// 					Use Your Room Safe or Reserve a Secure Locker Speaking of room
-// 					storage: If you have a room safe, use it to store anything you really
-// 					don’t want stolen. Yes, that means expensive jewelry and accessories –
-// 					if you brought them, and if they’re small enough to fit in the safe.
-// 					It also means your excess cash, credit and debit cards, and (when
-// 					abroad) essential travel documents like your passport and visa. For
-// 					obvious reasons, you don’t want to carry extra cash around in an
-// 					unfamiliar city, and losing your passport is a major inconvenience
-// 					that usually necessitates an in-person visit to the nearest embassy or
-// 					consulate.
-// 				</p>
-// 				<h2> Skip the International Calling Plan </h2>
-// 				<p>
-// 					Skip the International Calling Plan Unless you’re traveling for
-// 					business and absolutely need to have on-demand calling capabilities,
-// 					don’t bother with your wireless provider’s international calling plan.
-// 					They typically carry steep per-minute call fees – $0.20 and up – or
-// 					hefty per-day fees. My provider, Verizon, charges $5 per day, per line
-// 					in Canada and Mexico, and $10 per day, per line elsewhere. Use a free
-// 					or super-cheap talk and text app instead: WhatsApp, Line, Viber, or
-// 					any other app you prefer Most work wherever Wi-Fi is available, so you
-// 					shouldn’t need to connect to any local wireless networks. Check ahead
-// 					with your hotel or hostel to make sure you can get reliable Wi-Fi
-// 					there, ideally for free. This isn’t
-// 				</p>
-// 				<h2> Reach Out to Other Solo Travelers </h2>
-// 					<p>
-// 				Reach out to other solo travelers heading your way. Why waste time
-// 				finding your compatriots and breaking the ice when you can get that out
-// 				of the way ahead of time? The easiest way to find other singles in your
-// 				destination is to join an online group (Facebook groups tend to be the
-// 				most accessible and well-traveled) for solo travelers. Many groups, like
-// 				Girls LOVE Travel, cater specifically to women travelers. Use these
-// 				resources to poll more seasoned women travelers about their experiences
-// 				abroad, paying special attention to geographic areas to be avoided or
-// 				unfamiliar cultural norms to watch for. Don’t be afraid of sounding
-// 				paranoid or overcautious. 16. Register With Your Home Country’s
-// 				Diplomatic Corps When you travel abroad, register with your home
-// 				country’s diplomatic corps. For U.S. citizens, that’s the State
-// 				Department, the official arbiter of international travel safety for U.S.
-// 				citizens. This is especially important in countries known for crime or
-// 				political instability. Dozens of countries are covered in part or whole
-// 				by State Department travel warnings, including popular U.S. tourist
-// 				destinations like Mexico. You don’t need to show up at the embassy in
-// 				person unless you need assistance. Just reach out to the appropriate
-// 				contact before you leave the U.S. You can also register your trip with
-// 				the State Department before you depart – my wife and I did this before a
-// 				recent trip to Costa Rica, and we were shocked at how easy it was. If
-// 				any issues arise on your trip, local embassy or consulate staff will
-// 				know where you are and how to get in touch with you.
-// 			</p>
-// 				<h2>Share Your Itinerary With Friends and Family</h2>
-// 				<p> Share Your Itinerary With Friends and Family (Not on Social Media)
-// 				When you travel alone, even on a quick weekend jaunt to a relatively
-// 				safe U.S. city like Seattle or Portland, make sure someone else knows
-// 				where you’re going. This could be a sibling, a parent, a close friend,
-// 				or a domestic partner (if they’re not traveling with you). The most
-// 				important criteria: You trust them to have your back if and when you
-// 				need their help. Also, tempting as it may be, don’t announce solo
-// 				travels in advance on social media. You can’t be sure who’s checking
-// 				your posts. Wait to post pictures and reminiscences once you’re safely
-// 				back home.
-// 			</p>
-// 				</div>
-// 	)
-// }
-
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-
+export default function Safety() {
 	const useStyles = makeStyles((theme) => ({
-		root: {
-			maxWidth: 2550,
+		margin: {
+			margin: theme.spacing(1),
 		},
-		media: {
-			height: 10,
-			paddingTop: 200, 
-		},
-
 	}));
 
-	export default function Safety() {
-		const classes = useStyles();
+  return (
+		<div className="safetyContainer">
+			<div className="safetyTitle">
+				<h2 >Solo Travel Safety Precautions</h2>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle">Save Money</h2>
+				<p>
+					Each country has a high season, a low season, and in between
+					season.Those in between season are know as "shoulder seasons" and they
+					generally offer the ideal mix of affordability and comfort.Shoulder
+					seasons roughly coincide with spring and fall. the shoulder seasons
+					are the transitional periods between the rainy and dry seasons. Before
+					you make any decision of your destination, you should check the
+					climate
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle">Study Up on the Local Authorities</h2>
 
-		return (
-			<Card className={classes.root}>
-				<CardActionArea>
-					<CardMedia
-						className={classes.media}
-						image="/images/adventure-1850912_640.jpg"
-						title="Solo Travel Safety Precautions"
-					/>
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							Solo Travel Safety Precautions
-						</Typography>
-						<Typography variant="body2" color="textSecondary" component="p">
-							Lizards are a widespread group of squamate reptiles, with over
-							6,000 species, ranging across all continents except Antarctica
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary">
-						Share
-					</Button>
-					<Button size="small" color="primary">
-						Learn More
-					</Button>
-				</CardActions>
-			</Card>
-		);
-	}
+				<p>
+					Before you visit any country, do some research into the
+					trustworthiness of local civil authorities like police, emergency
+					services Before you arrive at your destination, check whether the
+					country or city you’re visiting has tourist police and plan
+					accordingly.
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle">Purchase Travel Insurance</h2>
+				<p>
+					Travel insurance is great for groups large and small, but it’s
+					especially helpful for solo travelers. If you’re planning to travel
+					abroad, especially if your itinerary involves long-distance hiking in
+					remote areas or other risky activities, make sure your policy has
+					medical evacuation coverage. Medical airlifts (air ambulances) are
+					incredibly expensive.
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle"> Protect Your Money and Valuables</h2>
+				<p>
+					Protecting Your Money and Valuables The first rule of preventing theft
+					while traveling abroad: Never carry more money than you need at one
+					time. If you’re spending two weeks abroad, don’t carry a two-week
+					supply of cash every time you leave your room. The second rule: Secure
+					your money, cards, and valuables in something other than a back pocket
+					or flimsy wallet. For cash, get a money belt that straps securely to
+					your waist and can’t easily be separated from your clothing. Quality
+					models start at $15 apiece. For your cards, get an RFID-blocking
+					wallet that stymies EMV chip card scanners, which high-tech
+					pickpockets use to lift credit card data from passersby in public
+					places. My RFID-blocking wallet cost less than $20 and looks great –
+					it’s not actual leather, but I certainly can’t tell the difference.
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle"> Be Discreet With Accessories </h2>
+				<p>
+					Flashing shiny, spendy accessories is a surefire way to attract the
+					wrong kind of attention. Avoid displaying expensive-looking jewelry,
+					handbags, sunglasses, and other accessories on the road. In fact,
+					unless you’re traveling for business and need to dress to impress,
+					there’s little reason to even bring such accessories on your trip. If
+					you do need to sport name brands at an evening function or two, that’s
+					fine – just dress down for the rest of your trip and leave the
+					expensive stuff tucked safely away in your room.
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle">Reserve Secure Room Locker </h2>
+				<p>
+					Use Your Room Safe or Reserve a Secure Locker Speaking of room
+					storage: If you have a room safe, use it to store anything you really
+					don’t want stolen. Yes, that means expensive jewelry and accessories –
+					if you brought them, and if they’re small enough to fit in the safe.
+					It also means your excess cash, credit and debit cards, and (when
+					abroad) essential travel documents like your passport and visa. For
+					obvious reasons, you don’t want to carry extra cash around in an
+					unfamiliar city, and losing your passport is a major inconvenience
+					that usually necessitates an in-person visit to the nearest embassy or
+					consulate.
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle">
+					{" "}
+					Skip the International Calling Plan{" "}
+				</h2>
+				<p>
+					Skip the International Calling Plan Unless you’re traveling for
+					business and absolutely need to have on-demand calling capabilities,
+					don’t bother with your wireless provider’s international calling plan.
+					They typically carry steep per-minute call fees – $0.20 and up – or
+					hefty per-day fees. My provider, Verizon, charges $5 per day, per line
+					in Canada and Mexico, and $10 per day, per line elsewhere. Use a free
+					or super-cheap talk and text app instead: WhatsApp, Line, Viber, or
+					any other app you prefer Most work wherever Wi-Fi is available, so you
+					shouldn’t need to connect to any local wireless networks. Check ahead
+					with your hotel or hostel to make sure you can get reliable Wi-Fi
+					there, ideally for free. This isn’t
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle"> Reach Out to Other Solo Travelers </h2>
+				<p>
+					Reach out to other solo travelers heading your way. Why waste time
+					finding your compatriots and breaking the ice when you can get that
+					out of the way ahead of time? The easiest way to find other singles in
+					your destination is to join an online group (Facebook groups tend to
+					be the most accessible and well-traveled) for solo travelers. Many
+					groups, like Girls LOVE Travel, cater specifically to women travelers.
+					Use these resources to poll more seasoned women travelers about their
+					experiences abroad, paying special attention to geographic areas to be
+					avoided or unfamiliar cultural norms to watch for. Don’t be afraid of
+					sounding paranoid or overcautious. 16. Register With Your Home
+					Country’s Diplomatic Corps When you travel abroad, register with your
+					home country’s diplomatic corps. For U.S. citizens, that’s the State
+					Department, the official arbiter of international travel safety for
+					U.S. citizens. This is especially important in countries known for
+					crime or political instability. Dozens of countries are covered in
+					part or whole by State Department travel warnings, including popular
+					U.S. tourist destinations like Mexico. You don’t need to show up at
+					the embassy in person unless you need assistance. Just reach out to
+					the appropriate contact before you leave the U.S. You can also
+					register your trip with the State Department before you depart – my
+					wife and I did this before a recent trip to Costa Rica, and we were
+					shocked at how easy it was. If any issues arise on your trip, local
+					embassy or consulate staff will know where you are and how to get in
+					touch with you.
+				</p>
+			</div>
+			<div className="safetyCard">
+				<h2 className="safetySubTitle">
+					Share Your Itinerary With Friends and Family
+				</h2>
+				<p>
+					Share Your Itinerary With Friends and Family (Not on Social Media)
+					When you travel alone, even on a quick weekend jaunt to a relatively
+					safe U.S. city like Seattle or Portland, make sure someone else knows
+					where you’re going. This could be a sibling, a parent, a close friend,
+					or a domestic partner (if they’re not traveling with you). The most
+					important criteria: You trust them to have your back if and when you
+					need their help. Also, tempting as it may be, don’t announce solo
+					travels in advance on social media. You can’t be sure who’s checking
+					your posts. Wait to post pictures and reminiscences once you’re safely
+					back home.
+				</p>
+			</div>
+		</div>
+	);
+}
+
+// import React from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Card from "@material-ui/core/Card";
+// import CardActionArea from "@material-ui/core/CardActionArea";
+// import CardActions from "@material-ui/core/CardActions";
+// import CardContent from "@material-ui/core/CardContent";
+// import CardMedia from "@material-ui/core/CardMedia";
+// import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
+
+// 	const useStyles = makeStyles((theme) => ({
+// 		root: {
+// 			maxWidth: 2550,
+// 		},
+// 		media: {
+// 			height: 10,
+// 			paddingTop: 200, 
+// 		},
+
+// 	}));
+
+// 	export default function Safety() {
+// 		const classes = useStyles();
+
+// 		return (
+// 			<Card className={classes.root}>
+// 				<CardActionArea>
+// 					<CardMedia
+// 						className={classes.media}
+// 						image="/images/adventure-1850912_640.jpg"
+// 						title="Solo Travel Safety Precautions"
+// 					/>
+// 					<CardContent>
+// 						<Typography gutterBottom variant="h5" component="h2">
+// 							Solo Travel Safety Precautions
+// 						</Typography>
+// 						<Typography variant="body2" color="textSecondary" component="p">
+// 							Lizards are a widespread group of squamate reptiles, with over
+// 							6,000 species, ranging across all continents except Antarctica
+// 						</Typography>
+// 					</CardContent>
+// 				</CardActionArea>
+// 				<CardActions>
+// 					<Button size="small" color="primary">
+// 						Share
+// 					</Button>
+// 					<Button size="small" color="primary">
+// 						Learn More
+// 					</Button>
+// 				</CardActions>
+// 			</Card>
+// 		);
+// 	}
 
