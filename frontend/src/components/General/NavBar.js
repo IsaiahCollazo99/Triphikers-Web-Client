be7af18f05617cd20026fe087cf1bc64f811443e
bsihame <sihamebazi@pursuit.org>
2020-09-05 01:36:06 -0400

    make changes to navbar

--- frontend/src/components/General/NavBar.js
+++ frontend/src/components/General/NavBar.js
@@ -30,7 +30,7 @@ const NavBar = ( props ) => {
 	const history = useHistory();
 
 	const [firstName, setFirstName] = useState("");
-
+	
 	const getFirstName = async ( backOffTime = 1 ) => {
 		try {
 			if (currentUser) {
@@ -67,7 +67,7 @@ const NavBar = ( props ) => {
 						role="button"
 						aria-expanded="false"
 					>
-						Hi, {firstName}
+						HI, {firstName}
 					</a>
 					<div className="dropdown-menu">
 						<Link className="dropdown-item" to={`/user/${currentUser.id}`}>
