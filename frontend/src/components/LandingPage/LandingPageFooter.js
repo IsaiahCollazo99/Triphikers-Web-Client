1e715c9a2fe1ce5bc976cfad1f202984b19f601e
jovannil94 <jovanniluna@pursuit.org>
2020-09-12 16:42:04 -0400

    fixes footer css

--- frontend/src/components/LandingPage/LandingPageFooter.js
+++ frontend/src/components/LandingPage/LandingPageFooter.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Whatsapp, Facebook, Twitter } from "react-social-sharing";
-import { NavLink } from "react-router-dom";
 import "../../css/landingPage/landingPageFooter.css";
 
 const LandingPageFooter = () => {
     return (
         <footer>
           <nav className="footer">
-            <NavLink className="footerLinks" exact to ="/terms&conditions">
-              Terms and Conditions
-            </NavLink>
-            <NavLink className="footerLinks" exact to ="/policy">
-              Privacy Policy
-            </NavLink>
             <p className="copyrightLine">© Triphikers LLC. 2020-2020. All rights reserved</p>
             <div className="shareSocial">
               <Twitter
