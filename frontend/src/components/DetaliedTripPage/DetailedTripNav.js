2a162ed5393acff77284f20bcd86e4f27754fb12
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-13 11:10:15 -0400

    add traveler & requests counter to dtn

--- frontend/src/components/DetaliedTripPage/DetailedTripNav.js
+++ frontend/src/components/DetaliedTripPage/DetailedTripNav.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../../css/detailedTripPage/detailedTripNav.css';
+import { getTripRequests, getTripTravelers } from '../../util/apiCalls/getRequests';
 
 const DetailedTripNav = ({ trip = {} }) => {
+    const [ requests, setRequests ] = useState([]);
+    const [ travelers, setTravelers ] = useState([]);
+
+    const getRequestsCall = async () => {
+        const data = await getTripRequests(trip.id)
+        if(data.requests) {
+            setRequests(data.requests);
+        } else {
+            setRequests([]);
+        }
+    }
+
+    const getTravelersCall = async () => {
+        const data = await getTripTravelers(trip.id);
+        if(data.travelers) {
+            setTravelers(data.travelers);
+        } else {
+            setTravelers([]);
+        }
+    }
+
+    useEffect(() => {
+        getTravelersCall();
+        getRequestsCall();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     
     return (
         <nav className="dt-nav">
-            <NavLink exact to={`/trips/${trip.id}`}>TRAVELERS</NavLink>
-            <NavLink exact to={`/trips/${trip.id}/requests`}>REQUESTS</NavLink>
+            <NavLink exact to={`/trips/${trip.id}`}>TRAVELERS ({travelers.length})</NavLink>
+            <NavLink exact to={`/trips/${trip.id}/requests`}>REQUESTS ({requests.length})</NavLink>
         </nav>
     )
 }
