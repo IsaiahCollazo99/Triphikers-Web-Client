0c8a244490c00761269a5fabb94cd1e12191f5ad
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-12 21:53:42 -0400

    move navbar in dtp

--- frontend/src/components/DetaliedTripPage/DetailedTripNav.js
+++ frontend/src/components/DetaliedTripPage/DetailedTripNav.js
@@ -1,24 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { AuthContext } from '../../providers/AuthContext';
 import '../../css/detailedTripPage/detailedTripNav.css';
 
 const DetailedTripNav = ({ trip = {} }) => {
-    const { currentUser } = useContext(AuthContext);
-
-    const displayRequests = () => {
-        if(currentUser.id === trip.planner_id) {
-            return <NavLink exact to={`/trips/${trip.id}/requests`}>REQUESTS</NavLink>
-        } else {
-            return null;
-        }
-    }
     
     return (
         <nav className="dt-nav">
-            <NavLink exact to={`/trips/${trip.id}`}>TRIP</NavLink>
-            <NavLink exact to={`/trips/${trip.id}/travelers`}>TRAVELERS</NavLink>
-            {displayRequests()}
+            <NavLink exact to={`/trips/${trip.id}`}>TRAVELERS</NavLink>
+            <NavLink exact to={`/trips/${trip.id}/requests`}>REQUESTS</NavLink>
         </nav>
     )
 }
